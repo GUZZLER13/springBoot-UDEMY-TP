9675c04c1f480b7c2fdaa53cc77e6125b31818e6
Stephane Da Silveira <sdasilveira@amiltone.com>
2022-07-21 15:50:04 +0200

    Exercice 21 - Afficher la liste des Films (Utiliser maintenant la page d'accueil de notre application Web pour afficher la liste des Films du catalogue) )

--- src/main/java/com/mycompany/dvdstore/repository/MovieRepositoryInterface.java
+++ src/main/java/com/mycompany/dvdstore/repository/MovieRepositoryInterface.java
@@ -2,6 +2,12 @@ package com.mycompany.dvdstore.repository;
 
 import com.mycompany.dvdstore.entity.Movie;
 
+import java.util.List;
+
 public interface MovieRepositoryInterface {
     void add(Movie movie);
+
+    List<Movie> list();
+
+
 }
--- src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
+++ src/main/java/com/mycompany/dvdstore/service/DefaultMovieService.java
@@ -5,18 +5,25 @@ import com.mycompany.dvdstore.repository.MovieRepositoryInterface;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DefaultMovieService implements MovieServiceInterface {
     @Autowired
 
-    MovieRepositoryInterface movieRepositoryInterface;
+    MovieRepositoryInterface movieRepository;
 
-    public void setMovieRepositoryInterface(MovieRepositoryInterface movieRepositoryInterface) {
-        this.movieRepositoryInterface = movieRepositoryInterface;
+    public void setMovieRepository(MovieRepositoryInterface movieRepository) {
+        this.movieRepository = movieRepository;
     }
 
     @Override
     public void registerMovie(Movie movie) {
-        movieRepositoryInterface.add(movie);
+        movieRepository.add(movie);
+    }
+
+    @Override
+    public List<Movie> getMovieList() {
+        return movieRepository.list();
     }
 }
